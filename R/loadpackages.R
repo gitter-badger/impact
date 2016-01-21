32a603aedbb34aed44ed563b6cdeee08d35cc359
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2016-01-20 22:25:35 -0500

    added README.md

---
+++ R/loadpackages.R
@@ -0,0 +1,41 @@
+#!/usr/bin/env Rscript
+#
+# Tim Sterne-Weiler
+# tim.sterne.weiler@utoronto.ca
+#
+#  Install/loading of required Packages.
+
+
+loadPackages <- function(toLoad, local.lib="Rlib/") {
+  for(i in toLoad) {
+    writeLines(sprintf("Trying to load required package: %s", toString(i)), stderr())
+    if(!suppressWarnings(require(toString(i), character.only=T, quietly=T)) &&
+                 !suppressWarnings(require(toString(i), character.only=T, lib.loc=local.lib, quietly=T))){
+        writeLines(sprintf("%s did not load correctly! Now trying to install..", i), stderr())
+        install.packages(i, dependencies=TRUE, repos='http://cran.us.r-project.org', lib=local.lib)
+        if(require(toString(i), character.only=T, lib.loc=local.lib)) {
+          writeLines(sprintf("%s has been installed locally in R/Rlib!", i), stderr())
+        } else {
+          stop(sprintf("quitting!!! I could not install %s for you!", i))
+                  }
+
+    }
+  }
+
+}
+
+#######
+# Colorblind Palette!
+
+cbb <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
+#######
+
+full.pkg.set <- c("cluster",
+	"apcluster",
+	"pamr",
+	"ggplot2",
+	"reshape2",
+	"gplots",
+	"RColorBrewer")
+
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # impact
 Meta data and code related to the IMPACT project.
+
+## Setup Instructions
+
+Choose a folder to put the git repository, open the terminal and `cd` to that directory.  Then clone the repository:
+```bash
+git clone git@github.com:timbitz/impact.git
+```
+
+Now link or copy the dropbox data directory into the git repo.
+```bash
+ln -s /Path/To/Dropbox/impact-proj/IMPACT_data
+```
+
+
