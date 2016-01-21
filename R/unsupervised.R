7377e782df80271d77d84f70660fa5ea648df2ba
Tim Sterne-Weiler <tim.sterne.weiler@utoronto.ca>
2016-01-21 14:24:42 -0500

    Update unsupervised.R

--- R/unsupervised.R
+++ R/unsupervised.R
@@ -1,10 +1,7 @@
 # This should contain multiple approaches towards unsupervised learning.
 #
 
-req.set <- c("cluster","pamr","apcluster","gplots")
+req.set <- c("cluster","pamr", "apcluster","gplots")
 loadPackages(req.set)
 
 
-
-euc.dist <- dist(log2fold, method = "euclidean")
-apres <- apcluster(euc.dist)
